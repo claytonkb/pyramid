b6860e4f37ddf60c50e795c8620e373c685e964f
Clayton Bauman <claytonkb@gmail.com>
2017-10-13 12:31:55 -0700

    work on compress*

--- src/compress.h
+++ src/compress.h
@@ -11,8 +11,8 @@ val8 compress_mtf_encode(pyr_cache *this_pyr, val8 in);
 char compress_mtf_encode_byte(char *byte_table, char *index_table, int curr_index, char c);
 void compress_mtf_update_tables(char *byte_table, char *index_table, int curr_index, char c);
 
-extern const uint32_t mtf_byte_table[MTF_BYTE_TABLE_SIZE];
-extern const uint32_t mtf_index_table[MTF_BYTE_TABLE_SIZE];
+extern const char mtf_byte_table[MTF_BYTE_TABLE_SIZE];
+//extern const char mtf_index_table[MTF_BYTE_TABLE_SIZE];
 
 #endif //COMPRESS_H
 
